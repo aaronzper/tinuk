52affe397422f7fa40d4df1a111fdfed157ffeea
skipper1931 <lordskipper1931@gmail.com>
2020-12-16 10:34:26 -0500

    Disabled all other cores
    BOOTBOOT had all the cores run the entry code at once, so i turned the other off so it doesn't screw us up

--- kernel/arch/x86_64/boot.asm
+++ kernel/arch/x86_64/boot.asm
@@ -7,6 +7,12 @@ EXTERN bootboot
 EXTERN fb
 
 _start:
+	mov rax, 1
+	cpuid 		; Call CPUID to get (among other things) the local APIC id for the core
+	shr ebx, 24	; Shift the APIC id into BX
+	cmp bx, [bootboot + 0xC] ; bootboot.bspid
+	jne _hang	; If we're not the boot CPU, hang
+
 	mov rdi, bootboot ; Load bootboot struct as first param to kmain()
 	mov rsi, fb ; Load VESA framebuffer addr as second param to kmain()
 	call kmain
