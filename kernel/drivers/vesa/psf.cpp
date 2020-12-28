f16dbba6c0a552820cb76ea521ddeeb4be970096
skipper1931 <lordskipper1931@gmail.com>
2020-12-28 18:40:48 -0500

    Fixed identation for other files

--- kernel/drivers/vesa/psf.cpp
+++ kernel/drivers/vesa/psf.cpp
@@ -3,32 +3,32 @@
 using namespace VESA;
 
 PSF::PSF(PSFHeader* h) {
-    if(psf_header->magic != PSF_magic) {
-        // Do something
-    }
+	if(psf_header->magic != PSF_magic) {
+		// Do something
+	}
 
-    psf_header = h;
+	psf_header = h;
 }
 
 PSFHeader PSF::header() {
-    return *psf_header;
+	return *psf_header;
 }
 
 unsigned char* PSF::operator[](char c) {
-    unsigned char* start = (unsigned char*)(psf_header + psf_header->headersize);
-    unsigned int index = (c > 0 && c < psf_header->numglyph ? c : 0) * psf_header->bytesperglyph;
+	unsigned char* start = (unsigned char*)(psf_header + psf_header->headersize);
+	unsigned int index = (c > 0 && c < psf_header->numglyph ? c : 0) * psf_header->bytesperglyph;
 
-    return &(start[index]);
+	return &(start[index]);
 }
 
 unsigned int PSF::bytesPerLine() {
-    return (psf_header->width + 7) / 8;
+	return (psf_header->width + 7) / 8;
 }
 
 unsigned int PSF::height() {
-    return psf_header->height;
+	return psf_header->height;
 }
 
 unsigned int PSF::width() {
-    return psf_header->width;
+	return psf_header->width;
 }
