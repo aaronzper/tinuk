d4a054226ea176f7f1c85dbccf87a2dedb163755
skipper1931 <lordskipper1931@gmail.com>
2020-12-29 17:07:30 -0500

    Printed CPU count and memory at boot

--- kernel/kmain.cpp
+++ kernel/kmain.cpp
@@ -15,7 +15,24 @@ extern "C" void kmain(BootbootInfo* bootboot, VESA::ARGB* fb, VESA::PSFHeader* p
 	white.blue = 255;
 
 	VESA::VESATerminal term(v, white, black, font);
-	io::set_terminal(&term);
+	IO::set_terminal(&term);
 
-	io::printk("Initialized VESA terminal\n");
+	IO::printk("Initialized VESA terminal\n");
+	IO::printk("Number of cores detected: %u\n", bootboot->numcores);
+
+	unsigned int mmap_entries = (bootboot->size - 128) / 16;
+	MMapEnt* mmap = &bootboot->mmap;
+	size_t total_mem = 0;
+	size_t unusable_mem = 0;
+	for(unsigned int i = 0; i < mmap_entries; i++) {
+		MMapEnt* entry = &mmap[i];
+		size_t size = MMapEnt_Size(entry);
+		total_mem += size;
+		if(!MMapEnt_IsFree(entry)) {
+			unusable_mem += size;
+		}
+	}
+
+	IO::printk("Total memory detected: %u mb\n", total_mem / 1024 / 1024);
+	IO::printk("Unusable memory detected: %u kb\n", unusable_mem / 1024);
 }
